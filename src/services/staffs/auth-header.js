669a5b9c6b25edda7b7e8cedb91fb210530d577e
Ackon Samuel <ackons433@gmail.com>
2022-12-18 07:02:26 +0000

    Get accesstoken and pass authorization header

---
+++ src/services/staffs/auth-header.js
@@ -0,0 +1,9 @@
+export default function staffAuthHeader() {
+    const staff = JSON.parse(localStorage.getItem('staff'));
+
+    if (staff && staff.accessToken) {
+        return { Authorization: 'Bearer' + staff.accessToken };
+    } else {
+        return {};
+    }
+}
