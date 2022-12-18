d23c10d93e5f4b782e63cb328129ac1c5eb76de0
Ackon Samuel <ackons433@gmail.com>
2022-12-18 07:06:46 +0000

    Hot fixes

--- src/services/staffs/staff.service.js
+++ src/services/staffs/staff.service.js
@@ -4,7 +4,8 @@ import staffAuthHeader from './auth-header';
 const API_URL_LOGOUT = 'http://localhost:3000/auth/staffs/logout';
 
 const logout = () => {
-    return axios.get(API_URL_LOGOUT, { headers: staffAuthHeader() });
+    localStorage.removeItem('staff');
+    return axios.delete(API_URL_LOGOUT, { headers: staffAuthHeader() });
 };
 
 export default logout;
