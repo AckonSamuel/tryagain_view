f51a7842b5bfd74625ea19c623b4264fe99ec881
Ackon Samuel <ackons433@gmail.com>
2022-12-18 07:04:13 +0000

    Create login and signup services

---
+++ src/services/staffs/auth.service.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const API_URL = 'https://localhost:3000/auth/staffs/';
+
+const register = (staff_name, email, phone_number, password, role, password_confirmation) => (
+    axios.post(API_URL + "signup", {
+        staff_name,
+        email,
+        password,
+        password_confirmation,
+        role,
+        phone_number
+    })
+);
+
+const login = ( email, password ) => (
+    axios.post(API_URL + 'login', {
+        email,
+        password
+    })
+    .then((res) => {
+        if (res.data.accessToken) {
+            localStorage.setItem('staff', JSON.stringify(response.data))
+        }
+
+        return response.data;
+    })
+)
+
+// const logout = () =>  {
+//     axios.delete(API_URL + 'logout')
+//     localStorage.removeItem('staff');
+// };
+
+const authService = {
+    register,
+    login,
+};
+
+export default authService;
