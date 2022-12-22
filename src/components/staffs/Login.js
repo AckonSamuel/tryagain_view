e2664174183404e09b0743d0de7740d4391f3b19
Ackon Samuel <ackons433@gmail.com>
2022-12-22 23:08:42 +0000

    Refactor staff login form

--- src/components/staffs/Login.js
+++ src/components/staffs/Login.js
@@ -1,16 +1,55 @@
-import React from 'react';
-import { useDispatch, useSelector, shallowEqual } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import * as Yup from "yup";
+import { yupResolver } from '@hookform/resolvers/yup';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import FormControl from '@mui/material/FormControl';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
 import { staffLogin } from '../../redux/slices/staffs/loginSlice';
 import MessageAlert from './ErrorAlert';
 
+const formSchema = Yup.object({
+    password: Yup.string()
+      .required("Password is required"),
+    email: Yup.string()
+     .required("Email is required"),
+  }).required();
+
 export default function Login() {
-    const { register, getValues, handleSubmit } = useForm();
     let navigate = useNavigate();
     const dispatch = useDispatch();
 
-    const showdata = () => { 
+    const [showPassword, setShowPassword] = useState(false);
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (e) =>  {
+        e.preventDefault();
+    };
+
+    let password;
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        watch,
+        getValues
+      } = useForm({
+        mode: "onTouched",
+        resolver: yupResolver(formSchema)
+      });
+
+      password = watch("password", "");
+
+      const showdata = () => { 
         const data = getValues();
         dispatch(staffLogin(data)).then(() => {
             navigate("/error");
@@ -19,13 +58,37 @@ export default function Login() {
     
 
     return (
-        <>
+<Box
+        sx={{ display: 'flex', flexWrap: 'wrap' }}
+        autoComplete='off'>
         <form onSubmit={handleSubmit(showdata)}>
-            <input {...register('email')} />
-            <input {...register('password')} />
-            <input type='submit' value='login' />
+            <TextField {...register('email')}  id='outlined-basic' label='email' variant='outlined' />
+            <p>{ errors.email ? errors.email.message : ''}</p>
+            <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined" >
+          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+            {...register('password')}
+          />
+          <p>{ errors.password ? errors.password.message : ''}</p>
+        </FormControl>
+            <Button variant="contained" type='sumbit' sx={{ margin: '10px'}} >Login</Button>
         </form>
-        </>
+        </Box>
 
     );
 }
