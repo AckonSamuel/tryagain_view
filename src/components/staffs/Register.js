43351780cef7684416ed947f07949afb8879e692
Ackon Samuel <ackons433@gmail.com>
2022-12-22 19:27:41 +0000

    Install yup and refactor password ui

--- src/components/staffs/Register.js
+++ src/components/staffs/Register.js
@@ -2,9 +2,18 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import FormControl from '@mui/material/FormControl';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
 import { staffSignUp } from '../../redux/slices/staffs/registerSlice';
 
 export default function Register() {
@@ -29,21 +38,58 @@ export default function Register() {
 
     return (
         <Box
-        sx={{ display: 'flex', flexWrap: 'wrap' }}
+        sx={{ display: 'flex', flexWrap: 'wrap', margin: '200px'}}
         notValidate
         autoComplete='off'>
         <form onSubmit={handleSubmit(showdata)}>
-            {/* <input {...register('email')} />
-            <input {...register('password')} />
-            <input  />
-            <input type='submit' value='login' /> */}
-            <TextField {...register('email')}  id='outlined-basic' label='email' variant='outlined' />
-            <TextField {...register('password')}  id='outlined-basic' label='Password' variant='outlined' />
-            <TextField {...register('password_confirmation')}  id='outlined-basic' label='Confirm Password' variant='outlined' />
-            <TextField {...register('role')}  id='outlined-basic' label='role' variant='outlined' />
-            <TextField {...register('name')}  id='outlined-basic' label='name' variant='outlined' />
-            <TextField {...register('phone_number')}  id='outlined-basic' label='Phone number' variant='outlined' />
-            <Button variant="contained" type='sumbit' >Register</Button>
+            <TextField {...register('email')}  id='outlined-basic' label='email' variant='outlined' sx={{ margin: '10px'}}/>
+            <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+            {...register('password')}
+          />
+        </FormControl>
+        <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">Confirm Password</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+            {...register('password_confirmation')}
+          />
+        </FormControl>
+            {/* <TextField {...register('password_confirmation')}  id='outlined-basic' label='Confirm Password' variant='outlined' sx={{ margin: '10px'}} /> */}
+            <TextField {...register('role')}  id='outlined-basic' label='role' variant='outlined' sx={{ margin: '10px'}} />
+            <TextField {...register('name')}  id='outlined-basic' label='name' variant='outlined' sx={{ margin: '10px'}} />
+            <TextField {...register('phone_number')}  id='outlined-basic' label='Phone number' variant='outlined' sx={{ margin: '10px'}} />
+            <Button variant="contained" type='sumbit' sx={{ margin: '10px'}} >Register</Button>
         </form>
         </Box>
     );
