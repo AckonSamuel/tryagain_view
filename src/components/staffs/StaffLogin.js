8341d16a739b55c4afdac3cb0532e204f7c1e21e
Ackon Samuel <ackons433@gmail.com>
2022-12-23 20:51:08 +0000

    Hot fixes

--- src/components/staffs/StaffLogin.js
+++ src/components/staffs/StaffLogin.js
@@ -29,7 +29,7 @@ function Copyright(props) {
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
       <Link color="inherit" href="#">
-        Your Website
+        KNUST Clubs and Societies platform
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
