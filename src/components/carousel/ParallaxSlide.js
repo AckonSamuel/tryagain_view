3862cceb5eb77e225fa4790a164cd7afd84cb3cd
Ackon Samuel <ackons433@gmail.com>
2022-12-31 13:59:26 +0000

    Construct parallaxSlide component

--- src/components/carousel/ParallaxSlide.js
+++ src/components/carousel/ParallaxSlide.js
@@ -8,3 +8,56 @@ const calculateMargin = (selfIndex, slideIndex, speed = 50) => {
     return `${diff * speed}%`;
 };
 
+const ParallaxSlide = ({ transition, children, renderElements, ...props }) => {
+    const [index, setIndex] = useState(0);
+    const [fineIndex, setFineIndex] = useState(index);
+    const onChangeIndex = i => {
+        setIndex(i);
+        setFineIndex(i);
+    };
+
+    const views = children({
+        fineIndex,
+        injectStyle: (slideIndex, speed) => ({
+            marginLeft: calculateMargin(slideIndex, fineIndex, speed),
+            transition: fineIndex === index ? transition : 'none',
+        }),
+    });
+
+    const isSingleView = views.length < 2;
+
+    return (
+        <>
+            <SwipeableViews
+                disabled={isSingleView}
+                resistance
+                springConfig={{
+                    duration: '0.6s',
+                    easeFunction: '',
+                    delay: '0s',
+                }}
+                enableMouseEvents
+                {...props}
+                index={index}
+                onChangeIndex={onChangeIndex}
+                onSwitching={i => {
+                    setFineIndex(i);
+                }}>
+                {views}
+            </SwipeableViews>
+            {!isSingleView && renderElements({ index, onChangeIndex })}
+        </>
+    );
+}
+
+ParallaxSlide.propTypes = {
+    children: PropTypes.func.isRequired,
+    transitions: PropTypes.string,
+    renderElements: PropTypes.func,
+};
+ParallaxSlide.defaultProps = {
+    transition: '0.8s',
+    renderElements: () => {},
+};
+
+export default ParallaxSlide;
