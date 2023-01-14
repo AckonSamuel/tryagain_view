5d0fc8a5df585ab22f0cc9d0502bc17ba980940a
Ackon Samuel <ackons433@gmail.com>
2023-01-14 20:15:25 +0000

    Fix linter error

--- src/layouts/authentication/components/BasicLayout/index.js
+++ src/layouts/authentication/components/BasicLayout/index.js
@@ -55,10 +55,7 @@ function BasicLayout({ image, children }) {
         </Grid>
       </MDBox> */}
       <MDBox px={1} width="100%" height="100vh" mx="auto">
-        <Grid container spacing={1}
-        justifyContent="center"
-        alignItems="center" 
-        height="100%">
+        <Grid container spacing={1} justifyContent="center" alignItems="center" height="100%">
           <Grid item xs={11} sm={9} md={5} lg={4} xl={3}>
             {children}
           </Grid>
